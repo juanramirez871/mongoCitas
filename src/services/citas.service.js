c7572c6fde5e72534ffacdd52968edad25ed5ea5
juanramirez871 <juandiegoramirez098@gmail.com>
2023-08-11 16:33:23 -0500

    feat: :construction: progreso enpoint 1

--- src/services/citas.service.js
+++ src/services/citas.service.js
@@ -1,11 +1,24 @@
 import { ObjectId } from "mongodb";
 import db from "../config/mongodb.js";
+const Users = db.getInstance().changeCollection("users").connect();
 
 export default class Citas {
 
 
     static async users(req, res){
 
-        return res.json("clientes");
+        const data = await Users
+            .find({})
+            .project({ attendant: 0, quotes: 0 })
+            .sort({ "names.name": 1 })
+            .toArray();
+
+        return res.json(data);
+    }
+
+    static async users(req, res){
+
+        const data = await Users.find().sort({ "names.name": 1 }).toArray();
+        return res.json(data);
     }
 }
